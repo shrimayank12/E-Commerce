5742c1be7efc69665fd91b607c2d636210446a7a
Mayank Shrivastava <shrimayank12@gmail.com>
2022-07-04 18:53:21 +0530

    added react-router 5.2.0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>Hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        Home
+      </Route>
+
+      <Route exact path="/category/:id">
+        Category
+      </Route>
+
+      <Route exact path="/checkout">
+        Checkout
+      </Route>
+
+      <Route>Error 404: Page Not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
