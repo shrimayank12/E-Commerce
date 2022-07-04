2b543cd49e24173f7d58d19e6f5507277da5ecec
Mayank Shrivastava <shrimayank12@gmail.com>
2022-07-04 20:41:28 +0530

    Added database files and displayed categories on Home Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { Container } from 'rsuite';
 import Navs from './components/Navs';
 import Category from './pages/Category';
 import Checkout from './pages/Checkout';
@@ -7,7 +8,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
+    <Container>
       <Navs />
 
       <Switch>
@@ -25,7 +26,7 @@ function App() {
 
         <Route>Error 404: Page Not Found</Route>
       </Switch>
-    </div>
+    </Container>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Col, Grid, Row } from 'rsuite';
+import categories from '../database/categories.json';
 
 const Home = () => {
-  return <div>This is Home Page</div>;
+  return (
+    <Grid>
+      <h1>Categories</h1>
+      <Row className="show-grid">
+        {categories.map(ctg => (
+          <Col key={ctg.id} sm={24} md={8}>
+            <Link to={`/category/${ctg.id}`}>
+              {' '}
+              <h2>{ctg.name}</h2>{' '}
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Grid>
+  );
 };
 
 export default Home;
